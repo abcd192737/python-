4cf90dcb7d1b5c0916209ab9139c31bfe7bdbeef
abcd192737 <abcd192737@gmail.com>
2019-05-24 22:21:22 +0800

    新增註解

--- urllib.py
+++ urllib.py
@@ -6,11 +6,11 @@ This is a temporary script file.
 """
 import urllib.request as ur #使用python內建urllib函式庫
 url='http://www.google.com'#使用Google來做測試
-connection=ur.urlopen(url)
-print(connection)
-mydata=connection.read()
+connection=ur.urlopen(url)#打開網頁
+print(connection)#印出蒐集到的資料
+mydata=connection.read()#讀取連接的資料並儲存於mydata
 print(mydata)
-print(connection.status)#網頁連接狀態
-print(connection.getheader('Content-Type'))
+print(connection.status)#status=網頁連接狀態
+print(connection.getheader('Content-Type'))#取得標頭檔
 for key,value in connection.getheaders():
-    print(key,value)
+    print(key,value)#印出迴圈
